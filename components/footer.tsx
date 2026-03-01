6da9736dc9adf86e5d23834cfc096c1d11d94c2e
mdKamrul-h <kamrul.sociofi@gmail.com>
2026-03-02 00:49:56 +0600

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -100,8 +100,8 @@ const Footer = () => {
               </li>
               <li className="flex items-center">
                 <Mail className="mr-2 h-5 w-5 text-primary-keppel" />
-                <a href="mailto:info@sociofitech.com" className="text-gray-300 hover:text-white transition-colors">
-                  info@sociofitech.com
+                <a href="mailto:info@sociofitechnology.com" className="text-gray-300 hover:text-white transition-colors">
+                  info@sociofitechnology.com
                 </a>
               </li>
             </ul>
